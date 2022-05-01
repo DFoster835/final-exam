743e9549e15118f84ae16fc36bd86c3e08316f59
DFoster835 <78561775+DFoster835@users.noreply.github.com>
2022-04-30 23:44:36 -0500

    Update games.js

--- backend/controllers/games.js
+++ backend/controllers/games.js
@@ -3,21 +3,29 @@ const Game = require('../models/games')
 
 gamesRouter.get('/', async (request, response) => {
 	// TODO: complete implementation for GET all
-	response.json([])
+	Game.find({}).then(result => {
+	response.json([result])
+	})
 })
 
 gamesRouter.post('/', async (request, response) => {
   const blog = new Game(request.body)
 	// TODO: implement input validation
-	// TODO: BONUS create new entry with async/await syntax
+  if (game === undefined)
   blog
     .save()
     .then(result => {
+	  console.log('success');
       response.status(201).json(result)
     })
 })
 
 // TODO: implement GET /:id for a specific game
+gamesRouter.get('/games/:id, async(req, res) => {
+		Game.findById(req.params.id).then(result => {
+	response.json(result);
+	})
+})
 
 // TODO: implement DELETE /:id to remove a game
 
