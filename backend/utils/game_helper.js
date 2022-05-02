389ccd4d836ec2073a0d7b83793b4cd281d3cbe1
DFoster835 <78561775+DFoster835@users.noreply.github.com>
2022-05-01 20:40:25 -0500

    Update game_helper.js

--- backend/utils/game_helper.js
+++ backend/utils/game_helper.js
@@ -1,8 +1,8 @@
 
-const isValidMove = n => {
+const isValidMove = (move) => {
 	// TODO: complete implementation
 	// HINT: Number has methods to check if a value is an integer
-	return false
+	return typeof move === 'number' && [0,1,2].includes(move)
 }
 
 /*
@@ -10,6 +10,17 @@ const isValidMove = n => {
 */
 const play = ({player, computer}) => {
 	// TODO: complete implementation
+	if (!isValidMove(player) || !isValidMove(computer)) return undefined;
+	
+	if (player === computer) {
+		return "draw"
+	}
+	else if (player % 3 !== ((computer + 1) % 3)) {
+		return "computer win"
+	}
+	else {
+		return "player win"
+	}
 }
 
 
