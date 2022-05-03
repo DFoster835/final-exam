5c1f63c9a689660b74b9ea2b3b9c91be665343b2
DFoster835 <78561775+DFoster835@users.noreply.github.com>
2022-05-03 04:44:32 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,13 @@
-function gameResults(props) {
- const games = props.games
- if(games && games.length > 0) {
-  
-  
- return (<>
-  <ul>
-   {games.map(function(game) {
-    return (<li>
-          <span>winner: {game,winner} </span>
-          <span>player played: {movesDict[game,moves,player]} </span>
-          <span>computer played: {movesDict[game,moves,computer]} </span>
-         </li>)
-      })}
-     </ul>
-  </>)
- }
-  
-}
-
-const gameData = [
-  {
-    "id": 1,
-    "winner": "player",
-    "moves": {
-      "player": 0,
-      "computer": 2
-    },
-    "playedAt": "Thu, 29 Apr 2021 22:50:27 GMT"
-  }
-]
+import './App.css';
+import head from './header_func';
+import playGame from './play_game';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Rock, Paper, Scissors</h1>
-      </header>
-    </div>
+    <>
+      <head/>
+      <playGame/>
+    </>
   );
 }
 
